ede7d8cf2e24e24c3a025a2ba99457fa781fe5db
sourabh-11 <sourabhjangid2002@gmail.com>
2022-12-21 11:27:37 +0530

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+const endDate = " 23 november 2023 12:00 AM"
+document.getElementById("end-date").innerHTML = endDate;
+const inputs =  document.querySelectorAll("input")
+
+
+function clock(){
+    const end =  new Date(endDate);
+    const  now =  new Date();
+    const diff =  (end-now)/1000;
+    if(diff<0) return;
+    // console.log(diff);
+ inputs[0].value =  (Math.floor(diff/24/3600));
+ inputs[1].value =  (Math.floor((diff/3600)%24));
+ inputs[2].value =  (Math.floor((diff/60)%60))
+ inputs[3].value =  (Math.floor(diff%60));
+
+    // 
+}
+clock();
+
+setInterval(()=>{
+    clock()
+},1000);
